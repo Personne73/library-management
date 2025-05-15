60dd0c598c1c6e4711d0f44b6090e07ff08f8efb
Joël Mbiapa <94489918+Personne73@users.noreply.github.com>
2025-05-16 01:03:35 +0200

    fix(lms): fix borrow repository

--- src/main/java/com/mooc/library_management/repository/BorrowRepository.java
+++ src/main/java/com/mooc/library_management/repository/BorrowRepository.java
@@ -3,4 +3,4 @@ package com.mooc.library_management.repository;
 import com.mooc.library_management.domain.Borrow;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BorrowRepository extends JpaRepository<Borrow,Integer> { }
+public interface BorrowRepository extends JpaRepository<Borrow,Long> { }
